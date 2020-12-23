e6aed7ffa64e150632bc75806d511d2fa4acc897
Marc Schraffenberger <marc@schraffenberger.com>
2020-12-23 09:41:27 -0500

    adding a gym map for testing

--- tools/common/cmdlib.c
+++ tools/common/cmdlib.c
@@ -34,7 +34,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <libc.h>
 #endif
 
-#define	BASEDIRNAME	"quake2"
+#define	BASEDIRNAME	"data"
 #define PATHSEPERATOR   '/'
 
 // set these before calling CheckParm
